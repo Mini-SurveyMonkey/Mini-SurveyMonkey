94e0c3a54802af5fd71effd3263b312a91cd3873
Mahad Mohamed Yonis <mahad.toukaleh56@gmail.com>
2025-11-08 14:48:57 -0500

    Implemented dynamic survey rendering (frontend + controller mapping)

--- src/main/java/org/example/controller/ViewController.java
+++ src/main/java/org/example/controller/ViewController.java
@@ -1,7 +1,9 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
@@ -14,5 +16,11 @@ public class ViewController {
     @GetMapping("/surveys/new")
     public String newSurvey() {
         return "surveys-create";
+    }
+
+    @GetMapping("/surveys/{id}/take")
+    public String takeSurvey(@PathVariable Long id, Model model) {
+        model.addAttribute("surveyId", id);
+        return "surveys-take";
     }
 }
